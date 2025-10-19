f9f154de75c006ad2f66937c0ace5cb00bf65391
DaviCarvalho <davicarvalho425@gmail.com>
2025-10-19 01:54:15 -0400

    feat: add the constant of the project

---
+++ app/components/constants.py
@@ -0,0 +1,18 @@
+"""
+CONSTANTS MUDULE
+
+"""
+ ##KABUM PATH
+KABUM_TITLE_PRODUCT_XPATH = '/html/body/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[4]/div[2]/div[2]/div[2]/div[1]/h2[1]'
+KABUM_AVISTA_CSS_SELECTOR = '.text-black-600.text-xs.font-normal.line-through'
+KABUM_PARCELADO_CSS_SELECTOR = '.text-4xl.text-secondary-500.font-bold.transition-all.duration-500'
+
+##PICHAU PATH
+PICHAU_TITLE_PRODUCT_CSS = '.MuiTypography-root.MuiTypography-h6.mui-1ri6pu6-product_info_title'
+PICHAU_AVISTA_CLASS_NAME = 'mui-1jk88bq-price_vista-extraSpacePriceVista'
+PICHAU_PARCELADO_CLASS_NAME = 'mui-7ie9un-price_total'
+
+##TERABYTE PATH
+TERABYTE_TITLE_PRODUCT_XPATH = '/html/body/div[4]/div[2]/div/div/div[3]/div/div/h1'
+TERABYTE_AVISTA_ID = 'valVista'
+TERABYTE_PARCELADO_XPATH = '/html/body/div[4]/div[2]/div/div/div[3]/div/div/div[10]/div[2]/div[3]/div[1]/p/span[1]'
