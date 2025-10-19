34e977024d2d45e7e1be2f722ed5ac31844c20c0
DaviCarvalho <davicarvalho425@gmail.com>
2025-10-19 01:54:48 -0400

    feat: module that scrap the prices

---
+++ app/components/scrap_method.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from app.components.constants import KABUM_AVISTA_CSS_SELECTOR,KABUM_PARCELADO_CSS_SELECTOR,PICHAU_AVISTA_CLASS_NAME, PICHAU_PARCELADO_CLASS_NAME,TERABYTE_AVISTA_ID,TERABYTE_PARCELADO_XPATH, KABUM_TITLE_PRODUCT_XPATH,PICHAU_TITLE_PRODUCT_CSS,TERABYTE_TITLE_PRODUCT_XPATH
+
+service = Service(ChromeDriverManager().install())
+
+
+class ScrapPrices():
+    
+    def __init__(self):
+        self.driver = webdriver.Chrome(service=service)
+        self.valores_vista = []
+        self.valores_parcelado = {}
+       
+ 
+    def kabum_scrap(self,url):
+        
+        self.driver.get(url)
+        product_title = self.driver.find_element(By.XPATH,KABUM_TITLE_PRODUCT_XPATH)
+        title = product_title.text
+        time.sleep(2)
+        element_cash = self.driver.find_element(By.CSS_SELECTOR,KABUM_AVISTA_CSS_SELECTOR)
+        cash = element_cash.text
+        time.sleep(2)
+        element_installment = self.driver.find_element(By.CSS_SELECTOR,KABUM_PARCELADO_CSS_SELECTOR)
+        installment = element_installment.text
+        
+        
+        return cash,installment,title
+    
+    def pichau_scrap(self,url):
+       
+        self.driver.get(url)
+        product_title = self.driver.find_element(By.CSS_SELECTOR,PICHAU_TITLE_PRODUCT_CSS)
+        title = product_title.text
+        element_cash = self.driver.find_element(By.CLASS_NAME, PICHAU_AVISTA_CLASS_NAME)
+        cash = element_cash.text
+        element_installment = self.driver.find_element(By.CLASS_NAME, PICHAU_PARCELADO_CLASS_NAME)
+        installment = element_installment.text
+    
+        return cash,installment,title
+    
+
+    def terabyte_scrap(self,url):
+        
+        self.driver.get(url)
+        
+        
+        time.sleep(5)
+            # Fecha o banner, se existir
+        self.driver.find_element(By.XPATH, '//*[@id="bannerPop"]/div/div/button/span').click() 
+        time.sleep(5)
+        
+  
+        product_title = self.driver.find_element(By.XPATH,TERABYTE_TITLE_PRODUCT_XPATH)
+        title = product_title.text
+        time.sleep(2)
+        element_cash = self.driver.find_element(By.ID,TERABYTE_AVISTA_ID)
+        cash = element_cash.text
+        time.sleep(2)
+        element_installment = self.driver.find_element(By.XPATH,TERABYTE_PARCELADO_XPATH)
+        installment = element_installment.text
+
+        return cash, installment,title
