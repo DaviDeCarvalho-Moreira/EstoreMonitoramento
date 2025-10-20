3c033f5200d2d05c55060d439952affdcbcd704e
DaviCarvalho <davicarvalho425@gmail.com>
2025-10-20 09:16:56 -0400

    feat: data orms

---
+++ app/models/products.py
@@ -0,0 +1,27 @@
+from app.database.Base import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String,DateTime,Integer
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from typing import List
+
+
+class Product(Base):
+    __tablename__ = "product"
+    
+    product_id: Mapped[int] = mapped_column(Integer,primary_key=True)
+    insert_date: Mapped[datetime] = mapped_column(DateTime,default=datetime.now)
+    product_name: Mapped[str] = mapped_column(String(200))
+    product_url: Mapped[str] = mapped_column(String(200))
+    store_id: Mapped[int] = mapped_column(Integer,ForeignKey("store.user_id"))
+    
+    store: Mapped["Store"] = relationship(back_populates="products")
+    
+    products: Mapped[List["Price"]] = relationship(
+        back_populates="price",
+        cascade="all, delete-orphan"
+    )
+    
+    
